decb7ffe5143efdbf362d57285d567014989ac5e
Ankur Seth <iankurseth@gmail.com>
2022-02-04 03:32:46 +0530

    c0 with cra, tailwind and cleanup

---
+++ README.md
@@ -0,0 +1,9 @@
+
+# `portfolio`
+Lets build a portfolio.
+
+
+## `0.1.0` - `2022-02-04`
+* `cra`, `tailwind` and `cleanup`
+
+
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+
+
+function App () {
+	return (
+		<div className="App">
+			<div className="flex bg-red-500 text-white min-h-screen">
+				<h1 className="m-auto text-[100px]">x12</h1>
+			</div>
+		</div>
+	);
+}
+
+export default App;
